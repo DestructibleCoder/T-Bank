b6a14a6648c9c48f428300e63cb7e050992af4a6
Popov Roman _Win <romanpopov2021@gmail.com>
2025-04-27 15:33:00 +0300

    continue on laptop

--- App/routes.py
+++ App/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from App import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Welcome to start page!"
+    data = {'title': 'edulog', 'name': 'Roman'}
+    return render_template('index.html', data=data)
